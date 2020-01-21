ba03247bee4395205d187e434d37db8653184357
Smurf136 <apiwat.it24@mail.kmutt.ac.th>
2020-01-21 13:18:07 +0700

    [Init] initial commit

---
+++ src/main/java/wipcamp12/line/bot/linebotwipcamp12/LineBotWipCamp12Application.java
@@ -0,0 +1,13 @@
+package wipcamp12.line.bot.linebotwipcamp12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LineBotWipCamp12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LineBotWipCamp12Application.class, args);
+	}
+
+}
---
+++ src/test/java/wipcamp12/line/bot/linebotwipcamp12/LineBotWipCamp12ApplicationTests.java
@@ -0,0 +1,13 @@
+package wipcamp12.line.bot.linebotwipcamp12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LineBotWipCamp12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
